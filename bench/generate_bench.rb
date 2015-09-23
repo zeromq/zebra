fe74bca406c9694a1ca60452da3b3b5b8bb23b4b
Kevin Sapper <k.sapper@eckelmann.de>
2015-09-23 05:23:14 +0000

    Problem: Benchmark not part of zwebrap Solution: Move them to another project
    git-svn-id: https://svn.eckelmann.group/svn/kgl/LDS/Common/zwebrap/trunk@14970 8cdf5627-b312-f249-8d72-5cdd174cf025

--- bench/generate_bench.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-arr    = ["foo", "bar", "baz", "qux", "quux", "corge", "grault", "garply"]
-paths  = arr.permutation(6).map { |a| "/#{a.join '/'}" }
-paths.each_with_index do |path, index|
-    # puts "    ztrie_insert_route (n, #{path}, NULL, NULL);"
-    puts "    zhash_insert (n, \"#{path}\", NULL);"
-end
