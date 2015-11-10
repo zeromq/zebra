9658dc64228aa19475c591b3a1422da3ff214096
Kevin Sapper <mail@kevinsapper.de>
2015-11-10 12:25:39 +0100

    Problem: ci_build fails as libmicrohttd is not being installed Solution: download, build and install libmicrohttd

--- ci_build.sh
+++ ci_build.sh
@@ -14,6 +14,11 @@ if [ $BUILD_TYPE == "default" ]; then
     CONFIG_OPTS+=("PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig")
     CONFIG_OPTS+=("--prefix=${BUILD_PREFIX}")
 
+    # Download and build libmicrohttpd
+    wget http://ftpmirror.gnu.org/libmicrohttpd/libmicrohttpd-0.9.38.tar.gz
+    tar -xvf libmicrohttpd-0.9.38.tar.gz
+    ( cd libmicrohttpd-0.9.38 && ./configure "${CONFIG_OPTS[@]}" && make check && make install ) || exit 1
+
     # Clone and build dependencies
     git clone https://github.com/zeromq/libzmq zmq
     ( cd zmq && ./autogen.sh && ./configure --without-libsodium "${CONFIG_OPTS[@]}" && make check && make install ) || exit 1
