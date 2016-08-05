ee2c6a486ec7778f48e9885f1aebd9a9499630ce
Kevin Sapper <mail@kevinsapper.de>
2016-08-05 09:42:42 +0000

    Problem: There is no test case for PUT
    Solution: Write a new PUT tests and use better comments

--- src/zeb_microhttpd.c
+++ src/zeb_microhttpd.c
@@ -739,7 +739,8 @@ zeb_microhttpd_test (bool verbose)
 #if defined (HAVE_LIBCURL)
     int rc = 0;
     //  @selftest
-    //  Simple create/destroy test
+    //  Start and configure http server and broker
+    //  ------------------------------------------
     zactor_t *zeb_microhttpd = zactor_new (zeb_microhttpd_actor, NULL);
 
     zstr_send (zeb_microhttpd, "START");
@@ -764,7 +765,8 @@ zeb_microhttpd_test (bool verbose)
         zstr_send (broker, "VERBOSE");
     zstr_sendx (broker, "BIND", "inproc://http_broker", NULL);
 
-    //  Create handler
+    //  Create a XRAP hander and connect it to broker
+    //  ---------------------------------------------
     zeb_client_t *handler = zeb_client_new ();
     assert (handler);
 
@@ -776,6 +778,9 @@ zeb_microhttpd_test (bool verbose)
     //  Provide GET Offering
     rc = zeb_client_set_handler (handler, "GET", "/foo/{[^/]}");
     assert (rc == 0);
+
+    //  Simple GET example
+    //  ------------------
 
     //  Send GET Request
     zeb_curl_client_t *curl = zeb_curl_client_new ();
@@ -803,6 +808,9 @@ zeb_microhttpd_test (bool verbose)
     zeb_curl_client_verify_response (curl, 200, "Hello World!");
     zeb_curl_client_destroy (&curl);
 
+    //  Simple GET not found example
+    //  ----------------------------
+
     //  Send GET Request 2
     curl = zeb_curl_client_new ();
     zeb_curl_client_send_get (curl, "http://localhost:8081/foo/bar/baz");
@@ -810,6 +818,9 @@ zeb_microhttpd_test (bool verbose)
     //  Receive GET Response 2
     zeb_curl_client_verify_response (curl, 404, PAGE_NOT_FOUND);
     zeb_curl_client_destroy (&curl);
+
+    //  Simple POST example
+    //  -------------------
 
     //  Provide POST Offering
     rc = zeb_client_set_handler (handler, "POST", "/foo/{[^/]}");
@@ -847,6 +858,45 @@ zeb_microhttpd_test (bool verbose)
     zeb_curl_client_verify_response (curl, 201, "Hello World!");
     zeb_curl_client_destroy (&curl);
 
+    //  Simple PUT example
+    //  ------------------
+
+    //  Provide PUT Offering
+    rc = zeb_client_set_handler (handler, "PUT", "/foo/{[^/]}");
+    assert (rc == 0);
+
+    curl = zeb_curl_client_new ();
+    zeb_curl_client_send_put (curl, "http://localhost:8081/foo/bar", "abc");
+
+    //  Receive Request
+    request = zeb_client_recv (handler);
+    assert (request);
+    xrap_msg = xrap_msg_decode (&request);
+    assert (xrap_msg_id (xrap_msg) == XRAP_MSG_PUT);
+    assert (streq (xrap_msg_content_type (xrap_msg), "text/plain"));
+    assert (streq (xrap_msg_content_body (xrap_msg), "abc"));
+    assert (streq ("/foo/bar", xrap_msg_resource (xrap_msg)));
+    xrap_msg_destroy (&xrap_msg);
+
+    //  Send Response
+    xrap_msg = xrap_msg_new (XRAP_MSG_POST_OK);
+    xrap_msg_set_status_code (xrap_msg, 200);
+    xrap_msg_set_location (xrap_msg, "/foo/bar");
+    xrap_msg_set_etag (xrap_msg, "a3fsd3");
+    xrap_msg_set_date_modified (xrap_msg, 0);
+    response = xrap_msg_encode (&xrap_msg);
+    zeb_client_deliver (handler, zeb_client_sender (handler), &response);
+    sender = zeb_client_sender (handler);
+    zuuid_destroy (&sender);
+
+    //  Give response time to arrive
+    zclock_sleep (250);
+
+    zeb_curl_client_verify_response (curl, 200, NULL);
+    zeb_curl_client_destroy (&curl);
+
+    //  Cleanup
+    //  -------
     zeb_client_destroy (&handler);
     zactor_destroy (&broker);
 
